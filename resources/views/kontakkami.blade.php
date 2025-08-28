0337290af306ed1b230bb299a68f4c66bc3d052d
Indra Ardika <indraardika@gmail.com>
2025-08-28 13:44:15 +0800

    baru

--- resources/views/kontakkami.blade.php
+++ resources/views/kontakkami.blade.php
@@ -21,9 +21,9 @@
             {{-- Kiri: Map + Alamat --}}
             <div class="map-container">
                 <iframe
-                    src="!1m18!1m12!1m3!1d3949.311205304852!2d113.70866627575523!3d-8.171369481897127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6943274ac3921%3A0xfc4510ffe97c1c55!2sJl.%20Nias%20No.10%2C%20Tegal%20Boto%20Lor%2C%20Sumbersari%2C%20Kec.%20Sumbersari%2C%20Kabupaten%20Jember%2C%20Jawa%20Timur%2068121!5e0!3m2!1sid!2sid!4v1756359677155!5m2!1sid!2sid"
-                    width=" 100%" height="300" style="border:0;" allowfullscreen="" loading="lazy">
-                </iframe>
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3949.311205304852!2d113.70866627575523!3d-8.171369481897127!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6943274ac3921%3A0xfc4510ffe97c1c55!2sJl.%20Nias%20No.10%2C%20Tegal%20Boto%20Lor%2C%20Sumbersari%2C%20Kec.%20Sumbersari%2C%20Kabupaten%20Jember%2C%20Jawa%20Timur%2068121!5e0!3m2!1sid!2sid!4v1756359677155!5m2!1sid!2sid"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"
+                    referrerpolicy="no-referrer-when-downgrade"></iframe>
 
                 <div class="alamat">
                     <h3>Alamat Kami</h3>
